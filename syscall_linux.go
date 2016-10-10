7596eff61b7112dffe1e16c7d96fa26720e780c8
Sergey Zhuravlev <sergey@divpro.ru>
2016-10-10 09:09:44 +0500

    Common codebase in syscall_linux.go has been pushed into a ioctl() function

--- syscall_linux.go
+++ syscall_linux.go
@@ -6,31 +6,45 @@ import (
 	"unsafe"
 )
 
+func ioctl(a1, a2, a3 uintptr) error {
+	if _, _, errno := syscall.Syscall(syscall.SYS_IOCTL, a1, a2, a3); errno != 0 {
+		return errno
+	}
+
+	return nil
+}
+
 func ifaceAddIoctl(req *ifReq, uid, gid int, persist bool) error {
 	file, err := os.OpenFile("/dev/net/tun", os.O_RDWR, 0)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
+
 	req.Flags |= syscall.IFF_TUN_EXCL
-	if _, _, err := syscall.Syscall(syscall.SYS_IOCTL, file.Fd(), uintptr(syscall.TUNSETIFF), uintptr(unsafe.Pointer(req))); err != 0 {
+
+	if err := ioctl(file.Fd(), uintptr(syscall.TUNSETIFF), uintptr(unsafe.Pointer(req))); err != nil {
 		return os.NewSyscallError("ioctl: TUNSETIFF", err)
 	}
+
 	if uid != -1 {
-		if _, _, err := syscall.Syscall(syscall.SYS_IOCTL, file.Fd(), uintptr(syscall.TUNSETOWNER), uintptr(uid)); err != 0 {
+		if err := ioctl(file.Fd(), uintptr(syscall.TUNSETOWNER), uintptr(uid)); err != nil {
 			return os.NewSyscallError("ioctl: TUNSETOWNER", err)
 		}
 	}
+
 	if gid != -1 {
-		if _, _, err := syscall.Syscall(syscall.SYS_IOCTL, file.Fd(), uintptr(syscall.TUNSETGROUP), uintptr(gid)); err != 0 {
+		if err := ioctl(file.Fd(), uintptr(syscall.TUNSETGROUP), uintptr(gid)); err != nil {
 			return os.NewSyscallError("ioctl: TUNSETGROUP", err)
 		}
 	}
+
 	if persist {
-		if _, _, err := syscall.Syscall(syscall.SYS_IOCTL, file.Fd(), uintptr(syscall.TUNSETPERSIST), 1); err != 0 {
+		if err := ioctl(file.Fd(), uintptr(syscall.TUNSETPERSIST), 1); err != nil {
 			return os.NewSyscallError("ioctl: TUNSETPERSIST", err)
 		}
 	}
+
 	return nil
 }
 
@@ -40,24 +54,30 @@ func ifaceDelIoctl(req *ifReq) error {
 		return err
 	}
 	defer file.Close()
-	if _, _, err := syscall.Syscall(syscall.SYS_IOCTL, file.Fd(), uintptr(syscall.TUNSETIFF), uintptr(unsafe.Pointer(req))); err != 0 {
+
+	if err := ioctl(file.Fd(), uintptr(syscall.TUNSETIFF), uintptr(unsafe.Pointer(req))); err != nil {
 		return os.NewSyscallError("ioctl: TUNSETIFF", err)
 	}
-	if _, _, err := syscall.Syscall(syscall.SYS_IOCTL, file.Fd(), uintptr(syscall.TUNSETPERSIST), 0); err != 0 {
+
+	if err := ioctl(file.Fd(), uintptr(syscall.TUNSETPERSIST), 0); err != nil {
 		return os.NewSyscallError("ioctl: TUNSETPERSIST", err)
 	}
+
 	return nil
 }
 
 func ifaceFeaturesIoctl() (uint16, error) {
-	var features uint16
 	file, err := os.OpenFile("/dev/net/tun", os.O_RDWR, 0)
 	if err != nil {
 		return 0, err
 	}
 	defer file.Close()
-	if _, _, err := syscall.Syscall(syscall.SYS_IOCTL, file.Fd(), uintptr(syscall.TUNGETFEATURES), uintptr(unsafe.Pointer(&features))); err != 0 {
+
+	var features uint16
+
+	if err := ioctl(file.Fd(), uintptr(syscall.TUNGETFEATURES), uintptr(unsafe.Pointer(&features))); err != nil {
 		return 0, os.NewSyscallError("ioctl: TUNGETFEATURES", err)
 	}
+
 	return features, nil
 }
